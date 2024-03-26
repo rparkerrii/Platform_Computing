de791fbf6ffcd25f1afa0de3ac72b96abd8daf6b
Robert Parker <rparkerrii@gmail.com>
2024-03-25 20:17:52 -0700

    committing assignment 3

---
+++ Assignment_3_Database/Metric-tracker/metric_tracker.js
@@ -0,0 +1,44 @@
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
+// Set up Chrome options
+const options = new chrome.Options();
+options.addArguments("--headless"); // Run Chrome headlessly
+
+// Initialize browser
+const driver = new Builder()
+  .forBrowser("chrome")
+  .setChromeOptions(options)
+  .build();
+
+// Navigate to your website
+driver.get("http://localhost:3000/");
+
+const metrics = [];
+// Track presence time
+const startTime = new Date().getTime() / 1000;
+let presenceTime = startTime;
+let numClicks = 0;
+const interval = setInterval(() => {
+  const currentTime = new Date().getTime() / 1000;
+  presenceTime = currentTime - startTime;
+  console.log(`Presence time: ${presenceTime} seconds`);
+
+  // Track scrolling
+  driver.executeScript("return document.body.scrollHeight").then((scrollHeight) => {
+    driver.executeScript("return window.pageYOffset").then((currentScroll) => {
+      console.log(`Scrolled ${currentScroll}/${scrollHeight} pixels`);
+    });
+  });
+
+  // Track clicks
+  driver.findElements(By.tagName("button")).then((buttons) => {
+     buttons.forEach((button) => {
+       button.click();
+       numClicks++;
+     });
+     console.log(`Number of clicks: ${numClicks}`);
+   });
+
+}, 2000);
+
---
+++ Assignment_3_Database/Metric-tracker/selenium_tutorial.js
@@ -0,0 +1,41 @@
+const {By, Builder, Browser} = require('selenium-webdriver');
+const assert = require("assert");
+
+(async function firstTest() {
+  let driver;
+  
+  try {
+    // Starts the session 
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    
+    // Navigate to the webpage provided
+    await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
+    
+    // Requests the browser information when opening
+    let title = await driver.getTitle();
+    assert.equal("Web form", title);
+  
+    // Waits to make sure the element provided is on the webpage
+    await driver.manage().setTimeouts({implicit: 500});
+  
+    // Finds an element to interact with
+    let textBox = await driver.findElement(By.name('my-text'));
+    let submitButton = await driver.findElement(By.css('button'));
+  
+    // Takes action on an element 
+    await textBox.sendKeys('Selenium');
+    await submitButton.click();
+  
+    let message = await driver.findElement(By.id('message'));
+    
+    // stores the element that was requested
+    let value = await message.getText();
+    assert.equal("Received!", value);
+  } catch (e) {
+    console.log(e)
+  } finally {
+    
+    // Ends the process which closes the webpage as well
+    await driver.quit();
+  }
+}())
